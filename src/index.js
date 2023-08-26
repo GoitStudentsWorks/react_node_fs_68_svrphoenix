0e0a133959b501604fb4ddbd429a6ac6878eb4bf
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-08-26 14:51:26 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/react_node_fs_68_svrphoenix">
+      <BrowserRouter basename="/react_node_fs_68_svrphoenix/">
         <CalendarProvider>
           <App />
         </CalendarProvider>
